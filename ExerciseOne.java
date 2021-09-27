8fa4a0eb04dc527b28916b134d3e81c5f52227dc
Viktor <darmokhvalviktor@gmail.com>
2021-09-27 10:25:32 +0300

    first commit

---
+++ ExerciseOne.java
@@ -0,0 +1,8 @@
+public class ExerciseOne {
+    static int a;
+    static char ch;
+    public static void main(String[] args){
+        System.out.println(a);
+        System.out.println(ch);
+    }
+}
